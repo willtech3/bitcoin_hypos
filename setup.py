9680b718230dfa2775ba89d4719fb691d33e9b21
William Lane <william.west.lane@gmail.com>
2020-05-17 17:15:36 -0500

    one day I'll get this one line right

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
   packages=find_packages(),
   package_data={'bitcoin_hypos.config': ['*.ini']},
   include_package_data=True,
-  install_requires = find_packages(["*.tests", "*.tests.*", "tests.*", "tests"]),
+  install_requires = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
   entry_points='''
       [console_scripts]
       hypo=bitcoin_hypos.scripts.hypo:run
